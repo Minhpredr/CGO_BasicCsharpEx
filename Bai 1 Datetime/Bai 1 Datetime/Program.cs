03addc558e98f098d3a6c522a3790a3182f63136
Vũ Đức Minh <vumninh@gmail.com>
2023-07-03 14:06:31 +0700

    Datetime ( bai1)
    Hien thi time

---
+++ Bai 1 Datetime/Bai 1 Datetime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1_Datetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+    }
+}
