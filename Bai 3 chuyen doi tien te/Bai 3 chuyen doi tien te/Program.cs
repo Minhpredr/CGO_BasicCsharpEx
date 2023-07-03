d86f123ca0e377e65aef2fe5992d211e23199ee8
Vũ Đức Minh <vumninh@gmail.com>
2023-07-03 21:28:47 +0700

    Bai 3 : chuyen doi tien te
    chuyen doi tien te

---
+++ Bai 3 chuyen doi tien te/Bai 3 chuyen doi tien te/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3_chuyen_doi_tien_te
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tigia = 23000;
+            Console.WriteLine("Enter dola: ");
+            string input=Console.ReadLine();
+            int dola = int.Parse(input);
+            Console.WriteLine("gia tri la: " + dola * tigia);
+            Console.ReadKey();
+        }
+    }
+}
+
