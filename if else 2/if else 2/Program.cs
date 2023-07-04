b61773430fabb7509078f189d3c05f26d6c0f206
Vũ Đức Minh <vumninh@gmail.com>
2023-07-04 20:42:40 +0700

    update buoi2(7/4/2023)
    commit

---
+++ if else 2/if else 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" nhap ngay sinh cua ban: ");
+            string input = Console.ReadLine();
+
+            DateTime dob = DateTime.Parse(input);
+            DateTime dob_year = new DateTime(DateTime.Now.Year, dob.Month, dob.Day);
+            if (dob_year > DateTime.Now)
+            {
+                Console.WriteLine("ngay sinh nhat sap toi con " + (dob_year - DateTime.Now).TotalDays);
+            }
+            else if (dob_year == DateTime.Now.Date)
+            {
+                Console.WriteLine("Happy birthdayyyyyyyyyyy");
+            }
+            else
+            {
+                dob_year = dob_year.AddYears(1);
+                Console.WriteLine("ngay sinh nhat sap toi con " + (dob_year - DateTime.Now).TotalDays);
+            }
+
+
+
+            //Console.WriteLine("dob: " + dob.ToString());
+            Console.ReadKey();
+            
+        }
+    }
+}
