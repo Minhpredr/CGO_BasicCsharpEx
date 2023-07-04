f00e3d1ab76b5710a2d60aa282bccaa3c905bcd2
Vũ Đức Minh <vumninh@gmail.com>
2023-07-04 14:58:51 +0700

    Bai 4 thuc hanh toan tu
    thuc hanh toan tu

---
+++ Bai 4 thuc hanh sd toan tu/Bai 4 thuc hanh sd toan tu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_4_thuc_hanh_sd_toan_tu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
+
