a9a4c55c5aabb2c7efc34c67ebadc047016ca98b
Vũ Đức Minh <vumninh@gmail.com>
2023-07-05 09:35:48 +0700

    bai 5 : giai pt bac nnhat

---
+++ giai pt bac nhat/giai pt bac nhat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giai_pt_bac_nhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giai pt bac nhat");
+            Console.WriteLine("dang pt a * x + b = 0");
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("The solution is: {0}!", solution);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("The solution is all x!");
+                }
+                else
+                {
+                    Console.Write("Nod solution!");
+                }
+            }
+
+        }
+    }
+}
