eb10f325d75d8d0ef963cb63fbe778300f5c8bbc
Vũ Đức Minh <vumninh@gmail.com>
2023-07-13 10:17:02 +0700

    DEMOO

---
+++ DEMO vietpt tinhucln/DEMO vietpt tinhucln/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO_vietpt_tinhucln
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int donvi;
+            for (i = 1; i <= 10; i++)
+            {
+                for (donvi = 1; donvi <= 10; donvi++)
+                {
+                    Console.WriteLine(i+"x"+donvi+"="+donvi*i);
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
+
