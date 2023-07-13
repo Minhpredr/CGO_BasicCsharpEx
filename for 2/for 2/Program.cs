09ae5a216c59691527c1f53bf7fef6ef02b67928
Vũ Đức Minh <vumninh@gmail.com>
2023-07-13 10:16:42 +0700

    for 2

---
+++ for 2/for 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace for_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int donvi = 5;
+            int i = 1;
+            while (i <= 10)
+            {
+                int product = donvi * i;
+                Console.WriteLine(donvi + "x" + i + "=" + product);
+                i -= 2;
+            }
+
+        }
+    }
+}
